9bafd6dd8324b7c392006155296038d3a0ae1b05
Ynda Jas <yndajas@gmail.com>
2021-10-02 23:55:30 +0100

    add "e" to exit choices

--- Ruby/lib/menu.rb
+++ Ruby/lib/menu.rb
@@ -2,8 +2,8 @@ class Algorithms::Menu
   def run
     puts "Welcome to algorithms!\n\n"
     choice = ''
-    until %w[exit quit q].include?(choice.downcase)
-      puts '> Choose an algorithm or type "Exit", "Quit" or "Q" to exit'
+    until %w[exit quit e q].include?(choice.downcase)
+      puts '> Choose an algorithm or type "exit", "quit", "e" or "q" to exit'
       puts "    1: spiral matrix\n\n"
       print '    >> '
       choice = gets.strip
