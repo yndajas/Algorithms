a9aa9f9791f65729ea72174bd675664f37bded7a
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-10-09 17:59:41 +0100

    add offer export class method

--- Ruby/lib/export.rb
+++ Ruby/lib/export.rb
@@ -3,4 +3,16 @@ class Algorithms::Export
     json = object.to_json
     File.write(filepath, json)
   end
+
+  def self.offer_export(object)
+    puts 'Export to JSON? (y/n)'
+    export = ''
+    export = gets.strip until %w[yes no y n].include?(export)
+    if %w[yes y].include?(export)
+      puts "\nEnter a filepath (including filename)"
+      filepath = gets.strip
+      json(object, filepath)
+      puts "\nFile successfully exported to \"#{filepath}\"\n"
+    end
+  end
 end
