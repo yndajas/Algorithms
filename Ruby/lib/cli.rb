89b65c4b0319a9078f49fc341745e774626bd133
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-10-09 18:00:24 +0100

    add first unique character to main CLI menu

--- Ruby/lib/cli.rb
+++ Ruby/lib/cli.rb
@@ -4,13 +4,17 @@ class Algorithms::CLI
     choice = ''
     until %w[exit quit e q].include?(choice.downcase)
       puts '> Choose an algorithm or type "exit", "quit", "e" or "q" to exit'
-      puts "    1: spiral matrix\n\n"
+      puts '    1: first unique character'
+      puts "    2: spiral matrix\n\n"
       print '    >> '
       choice = gets.strip
       puts
 
       case choice
       when '1'
+        Algorithms::FirstUniqueCharacter.new.run
+        puts
+      when '2'
         Algorithms::SpiralMatrix.new.run
         puts
       end
