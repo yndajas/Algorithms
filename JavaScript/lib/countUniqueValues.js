f248c8e318147d539e0d3154063225fbd57d95bc
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2022-03-11 00:22:25 +0000

    feat: count unique values algorithm

---
+++ JavaScript/lib/countUniqueValues.js
@@ -0,0 +1,15 @@
+export default const countUniqueValues = (values) => {
+  // sort the value and declare a variable to store last unique value
+  const sortedValues = values.sort()
+  let lastUniqueValue
+
+  // iterate through values, counting each that differs from the last
+  return sortedValues.reduce((uniqueValueCount, currentValue) => {
+    if (currentValue !== lastUniqueValue) {
+      lastUniqueValue = currentValue
+      return uniqueValueCount + 1
+    }
+
+    return uniqueValueCount
+  }, 0)
+}
